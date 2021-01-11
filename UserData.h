b046cbf8fdbe9de25777d5f0c63207b35cb79d28
PlumK <1285847284@qq.com>
2021-01-11 09:22:49 +0800

    添加退出登录函数

--- UserData.h
+++ UserData.h
@@ -9,6 +9,7 @@ public:
 	void saveData();
 	int login(string userName, string passWord);
 	int reg(string userName, string passWord);
+	int logout(string userName);
 	int deleteFriends(string userName, string FriendName);
 	int requestAddFriend(string userName, string password);
 	int confirmAddFriend(string userName, string friendName);
